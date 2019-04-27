5d79b29df7c837579cfbb1ba30517a633c808f0f
BrianLi <13250240950@163.com>
2019-04-28 06:30:41 +0800

    finish LR1.0

--- machine-learning/LogisticRegression.py
+++ machine-learning/LogisticRegression.py
@@ -9,15 +9,40 @@ import matplotlib.pyplot as plt
 def sigmoid(inX):
     return 1.0/(1 + np.exp(-inX))
 
-def gradDecent(origData, labels, learning_rate=0.001, n_iterations=20, lamda=0.1, show_cost=True):
+def Accuracy(npData, labels, Weights):
+
+    n_examples = npData.shape[0]
+
+    n_right = 0
+
+    for i in range(0, n_examples):
+
+        h = sigmoid(np.dot(npData[i,:], Weights))
+
+        if h >= 0.5:
+            Ypredict = 1
+        else:
+            Ypredict = 0
+
+        if Ypredict == labels[i,:]:
+            n_right += 1
+
+    accuracy = float(n_right)/float(n_examples)
+
+    print("Accuracy: ", accuracy*100, "%")
+
+
+
+def gradDecent(origData, labels, learning_rate=0.001, n_iterations=2000, lamda=1.0, show_cost=True):
 
     #特征缩放
-    npData = FeatureScaling(origData)
+    npData = np.ones((origData.shape[0], origData.shape[1]+1), dtype=float)
+    npData[:,1:] = FeatureScaling(origData)
 
     n_examples = npData.shape[0]
     n_features = npData.shape[1]
 
-    #将权重随机初始化
+    #将权重随机初始化，包括W0项，其X0 = 1
     Weights = np.random.rand(n_features, 1)
 
     Cost = np.ones(n_iterations)
@@ -30,26 +55,50 @@ def gradDecent(origData, labels, learning_rate=0.001, n_iterations=20, lamda=0.1
         # 计算损失值
         Cost[k] = -np.sum(np.multiply(labels, np.log(h)) + np.multiply((1.0 - labels), np.log(1.0 - h)))/n_examples
         # 加上正则化，防止过拟合
-        Weights = Weights * lamda - learning_rate * np.dot(npData.T, error)
+        Weights[0,:] = Weights[0,:] - learning_rate * np.dot(npData[:,0].T, error)
+        Weights[1:,:] = Weights[1:,:] * lamda - learning_rate * np.dot(npData[:,1:].T, error)
 
     if show_cost:
         plt.plot(range(0, n_iterations), Cost)
         plt.show()
 
+    # 绘制图
+    plotBestFit(npData, labels, Weights)
+
+    accuracy = Accuracy(npData, labels, Weights)
+
     return Weights
 
-def plotBestFit(npData, Weight):
+def plotBestFit(npData, labels, Weight):
 
     n_examples = npData.shape[0]
 
+    X0 = []; Y0 = []
+    X1 = []; Y1 = []
+
+    for i in range(0, n_examples):
+        if labels[i,:] == 0:
+            X0.append(npData[i, 1]); Y0.append(npData[i, 2])
+        else:
+            X1.append(npData[i, 1]); Y1.append(npData[i, 2])
+
+    x = np.arange(-5.0, 5, 0.1)
+    y = -(Weight[0] + Weight[1]*x)/Weight[2]
+
+
+    plt.scatter(X0, Y0, c='red', marker='^')
+    plt.scatter(X1, Y1, c='blue', marker="o")
+    plt.plot(x, y)
+    plt.show()
 
 
 if __name__ == '__main__':
 
     filename = './ML-SourceCode/Ch05/testSet.txt';
 
-    npData, labels = TXTtoNumpy(filename, lableState=True, Print=True)
+    origData, labels = TXTtoNumpy(filename, lableState=True, Print=True)
 
-    Weight = gradDecent(npData, labels, show_cost=False)
+    Weights = gradDecent(origData, labels, show_cost=False)
 
-    print(Weight)
+
+    print(Weights)
